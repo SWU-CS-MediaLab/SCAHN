d1243048675b765c418e6e9df831401ed5c4cf7a
SWU-CS-MediaLab <51917214+SWU-CS-MediaLab@users.noreply.github.com>
2020-04-15 15:46:51 +0800

    Update __init__.py

--- torchcmh/models/SCAHN/__init__.py
+++ torchcmh/models/SCAHN/__init__.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
-# @Time    : 2019/7/13
-# @Author  : Godder
-# @Github  : https://github.com/WangGodder
+
 from .resnet import resnet18, resnet34
 from .MSText import get_MS_Text
 
