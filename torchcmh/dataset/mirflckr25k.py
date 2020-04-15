2ccf4f5da149e1d2476ccfadb5e7956c2cc8435c
SWU-CS-MediaLab <51917214+SWU-CS-MediaLab@users.noreply.github.com>
2020-04-15 15:40:41 +0800

    Update mirflckr25k.py

--- torchcmh/dataset/mirflckr25k.py
+++ torchcmh/dataset/mirflckr25k.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
-# @Time    : 2019/6/27
-# @Author  : Godder
-# @Github  : https://github.com/WangGodder
+
 from .base import *
 from scipy import io as sio
 import numpy as np
