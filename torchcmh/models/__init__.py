97950376eee581d088cafa0607acb3db9bbf21d2
SWU-CS-MediaLab <51917214+SWU-CS-MediaLab@users.noreply.github.com>
2020-04-15 14:34:19 +0800

    Add files via upload

---
+++ torchcmh/models/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/7/21
+# @Author  : Godder
+# @Github  : https://github.com/WangGodder
+import os
+__version__ = '0.3.0'
+__author__ = 'Xinzhi Wang'
+from torchcmh.models.base import BasicModule
+
+
+abs_dir = os.path.abspath(os.path.dirname(__file__))
+
